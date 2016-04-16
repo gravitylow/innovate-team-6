4bc8024b5025e8bccbb67495a9fffd30322d2e76
ClarenceEdmondsII <clarence.edmonds2@gmail.com>
2016-04-16 11:51:00 -0400

    Update api.py

--- flask/api.py
+++ flask/api.py
@@ -27,7 +27,7 @@ def incident_subtype(itype):
 
 @app.route("/incidenttype/<int:maintype>", methods=['GET'])
 def incident_type(maintype):
-    cursor.execute("SELECT * FROM INCIDENT_TYPE WHERE = MAINTYPE" & maintype)
+    cursor.execute("SELECT * FROM INCIDENT_TYPE WHERE MAINTYPE = " & maintype)
     rows = cursor.fetchall()
     return json.dumps(rows, default=date_handler);
 
