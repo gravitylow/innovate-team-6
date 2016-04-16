1ea012df3c8b9f4dffbf2369bc02817fdc2cbd54
Adam Fendley <adam.fendley@gmail.com>
2016-04-16 14:13:40 -0400

    python and stuff

--- flask/api.py
+++ flask/api.py
@@ -25,7 +25,7 @@ def hello():
 def incidents():
     cursor.execute("SELECT * FROM INCIDENTS")
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incident_submit", methods=['POST'])
 def incidentSubmit():
@@ -46,62 +46,62 @@ def incidentSubmit():
                    """ + description + """,
                    """ + latitude + """,
                    """ + longitude + ")")
-    return "Yay!"; #return render_template('Submitted.html')
+    return "Yay!" #return render_template('Submitted.html')
 
 @app.route("/incident/<int:id>")
 def incidentByID(id):
     cursor.execute("SELECT * FROM INCIDENTS WHERE INC_ID = " + str(id))
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incidentsubtypes")
 def incident_subtypes():
     cursor.execute("SELECT * FROM INCIDENT_SUBTYPE")
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incidentsubtype_by_id/<int:id>")
 def incidentSubtypeByID(id):
     cursor.execute("SELECT * FROM INCIDENT_SUBTYPE WHERE IST_ID = " + str(id))
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incidentsubtype_by_typeid/<int:itype>")
 def incidentSubtypeByTypeID(itype):
     cursor.execute("SELECT * FROM INCIDENT_SUBTYPE WHERE IST_TYPEID = " + str(itype))
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incidenttype")
 def incidentType():
     cursor.execute("SELECT * FROM INCIDENT_TYPE")
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/incidenttype/<int:id>")
 def incidentTypeByID(id):
     cursor.execute("SELECT * FROM INCIDENT_TYPE WHERE INT_ID = " + str(id) + "'" )
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 #Severities
 @app.route("/severities")
 def severities():
     cursor.execute("SELECT * FROM SEVERITies")
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 
 @app.route("/severities/<int:sev_id>", methods=['GET'])
 def severitiesByID(sev_id):
     cursor.execute("SELECT * FROM SEVERITies WHERE SEV_ID = " + str(sev_id) + "'")
     rows = cursor.fetchall()
-    return json.dumps(rows, default=date_handler);
+    return json.dumps(rows, default=date_handler)
 
 @app.route("/severities/<int:sev_id>", methods=['DELETE'])
 def severitiesDeleteByID(sev_id):
     cursor.execute("DELETE FROM SEVERITies WHERE SEV_ID = " + str(sev_id))
-    return True;
+    return True
 
 if __name__ == "__main__":
     app.run()
