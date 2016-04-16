660357a7d6f463b0ed5afd9695b47cd80372a52e
Adam Fendley <adam.fendley@gmail.com>
2016-04-16 11:08:54 -0400

    Add database connection and incident type API

--- flask/api.py
+++ flask/api.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import MySQLdb
+import json
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+db = MySQLdb.connect(host="ec2-54-152-223-36.compute-1.amazonaws.com",
+                     user="root",
+                     passwd="alpine",
+                     db="IRS_DB")
+
+cursor = db.cursor()
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/incidenttype")
+def incident_type():
+    cursor.execute("SELECT * FROM INCIDENT_TYPE")
+    rows = cursor.fetchall()
+    return json.dumps(rows, default=date_handler);
 
 if __name__ == "__main__":
     app.run()
