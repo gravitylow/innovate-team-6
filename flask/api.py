75a2f7e9ca3b48944c8cec1de5ac8202e0b5e51a
Erick G. Hagstrom <erick.hagstrom@gmail.com>
2016-04-16 13:05:55 -0400

    rendering submitted page, I hope

--- flask/api.py
+++ flask/api.py
@@ -46,7 +46,7 @@ def incidentSubmit():
                    """ + description + """,
                    """ + latitude + """,
                    """ + longitude + ")")
-    return 'OK, thanks luv you bye'
+    return render_template('Submitted.html')
 
 @app.route("/incident/<int:id>")
 def incidentByID():
